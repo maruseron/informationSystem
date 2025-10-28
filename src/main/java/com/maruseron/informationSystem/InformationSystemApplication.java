343bb2149a42216e3d0a2d32c72ef1072c932b5e
maruseron <marcelorodfu@gmail.com>
2025-10-28 14:01:10 -0400

    add cors config
    burp glorp morp furrrrrrrrrrrp

--- src/main/java/com/maruseron/informationSystem/InformationSystemApplication.java
+++ src/main/java/com/maruseron/informationSystem/InformationSystemApplication.java
@@ -2,6 +2,10 @@ package com.maruseron.informationSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class InformationSystemApplication {
@@ -10,4 +14,15 @@ public class InformationSystemApplication {
 		SpringApplication.run(InformationSystemApplication.class, args);
 	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080",
+                                        "http://localhost:5173");
+            }
+        };
+    }
 }
