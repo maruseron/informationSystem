da8a5738cd2f3f2226d9958584b1b7a5c31b0465
maruseron <marcelorodfu@gmail.com>
2025-11-18 15:31:13 -0400

    attendance removal
    was unused for a while, removed it

--- src/main/java/com/maruseron/informationSystem/persistence/AttendanceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.maruseron.informationSystem.persistence;
-
-import com.maruseron.informationSystem.domain.entity.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {}
